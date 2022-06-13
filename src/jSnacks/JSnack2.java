a386e887c960615194764bf41a791cce397b7faa
giuseppemaresca <marescag146@gmail.com>
2022-06-13 11:06:02 +0200

    SecondoEsercizio

---
+++ src/jSnacks/JSnack2.java
@@ -0,0 +1,19 @@
+package jSnacks;
+import java.util.Scanner;
+public class JSnack2 {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int numero;
+		System.out.println("Inserisci un numero: ");
+		numero=scanner.nextInt();
+		if(numero%2==0) 
+		{
+         System.out.println("Il tuo numero e' "+ numero);
+       	}
+		else {
+			numero=numero+1;
+			System.out.println("Il tuo numero e'  " +numero);
+		}
+	}
+}
